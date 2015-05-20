232efe9fe909d95188853f8a2005f0898bfc21b0
Corran <corranharker@hotmail.co.uk>
2015-05-20 21:39:26 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -110,7 +110,7 @@
         blacklists: {
             Troll: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/troll.json",
             OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleOPlist.json",
-            NSFW: "https://rawgit.com/ZoltofLightning/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            LMAO: "https://rawgit.com/ZoltofLightning/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
             LOL: "https://rawgit.com/ZoltofLightning/basicBot-customization/master/blacklists/ExampleTrolllist.json"
         }
     }));
