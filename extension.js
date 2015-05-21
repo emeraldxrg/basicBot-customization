5b49dda3670a82199e60745fc467df5e93518332
ZoltofLightning <zoltoflightning@gmail.com>
2015-05-20 22:32:32 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,7 +33,7 @@
          }
 
          */
-        bot.commands.baconCommand = {
+        bot.commands.test2Command = {
             command: 'test2',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
@@ -45,7 +45,7 @@
                 }
             }
         };
-        bot.commands.baconCommand = {
+        bot.commands.testCommand = {
             command: 'test',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
